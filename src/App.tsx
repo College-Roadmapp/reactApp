c96933e38088d909c19fdd2affb01d94233f5fb7
Isabelle Bretl <bretli@oregonstate.edu>
2022-01-04 16:56:16 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './osu.css';
+import beaverLogo from './beaverLogo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="http://oregonstate.edu"><img src="http://oregonstate.edu/themes/osu/drupal8-osuhomepage/logo.svg" alt="Oregon State University"/></a>
+      <div id="site-title">
+      <a href=""><h1>College Roadmap App</h1></a>
+      </div>
+      <button id="menu" className="dropdown-toggle" type="button" data-toggle="dropdown">
+      <i className="fa fa-bars"></i>
+   
+      MENU
+      </button>
+      <ul className="dropdown-menu menu" role="menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Resources</a></li>
+      </ul>
+    </header>
+      <div>
+          <a
+            className="App-link"
+            href="https://catalog.oregonstate.edu/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Oregon State University's Academic Catalog
+          </a>
+      </div>
     </div>
   );
 }
