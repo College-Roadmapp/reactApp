577f8f867c0b6abe9a917ca36dc447c6ad98a904
Caden Verzino <verzinoc@oregonstate.edu>
2022-02-15 10:53:02 -0800

    links added to resources tab

--- src/component/resources.tsx
+++ src/component/resources.tsx
@@ -3,7 +3,33 @@ import React,{ useState, useEffect, Component, MouseEvent } from 'react';
 function Resources(){
     return(
         <body>
-        <div>Resources</div>
+        <h3>Helpful Links</h3>
+          <ul>
+            <li><a href="https://oregonstate.edu">Oregon State University</a></li>
+            <li><a href="https://catalog.oregonstate.edu/college-departments/">Colleges, Schools, Departments, Programs</a></li>
+            <li><a href="https://oregonstate.edu/academics">Undergraduate Programs</a></li>
+            <li><a href="https://catalog.oregonstate.edu/programs/">All Programs</a></li>
+            <li><a href="https://catalog.oregonstate.edu/earning-degrees/">Earning a Degree</a></li>
+            <li><a href="https://admissions.oregonstate.edu/find-your-major">Discover Majors</a></li>
+            <li><a href="https://classes.oregonstate.edu">Class Search</a></li>
+            <li><a href="https://catalog.oregonstate.edu">Course Catalog</a></li>
+            <li><a href="https://catalog.oregonstate.edu/earning-degrees/bcc/">Baccalaureate Courses</a></li>
+            <li><a href="https://catalog.oregonstate.edu/courses/">Course Descriptions</a></li>
+          </ul>
+        <h3>School Pages</h3>
+          <ul>
+            <li><a href="https://agsci.oregonstate.edu">College of Agricultural Science</a></li>
+            <li><a href="https://business.oregonstate.edu">College of Business</a></li>
+            <li><a href="https://ceoas.oregonstate.edu">College of Earth, Ocean, and Atmospheric Sciences</a></li>
+            <li><a href="https://education.oregonstate.edu">College of Education</a></li>
+            <li><a href="https://engineering.oregonstate.edu">College of Engineering</a></li>
+            <li><a href="https://www.forestry.oregonstate.edu">College of Forestry</a></li>
+            <li><a href="https://liberalarts.oregonstate.edu">College of Liberal Arts</a></li>
+            <li><a href="https://pharmacy.oregonstate.edu">College of Pharmacy</a></li>
+            <li><a href="https://health.oregonstate.edu">College of Public Health and Human Science</a></li>
+            <li><a href="https://science.oregonstate.edu">College of Science</a></li>
+            <li><a href="https://vetmed.oregonstate.edu">College of Veterinary Medicine</a></li>
+          </ul>
         </body>
     )
 }
