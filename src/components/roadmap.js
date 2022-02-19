4ae42b4ae9e030695787ab104b71a09f6e07738d
Isabelle Bretl <bretli@oregonstate.edu>
2022-02-19 13:22:23 -0800

    mapping dropdown items from a list instead of hardcoded

--- src/components/roadmap.js
+++ src/components/roadmap.js
@@ -4,7 +4,9 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Header from './header';
-import React from 'react';
+import React, { Component, useState } from 'react';
+// import { Menu } from '@mui/material';
+// import { render } from '@testing-library/react';
 
 
 function Roadmap() {
@@ -29,51 +31,73 @@ function AppDisplay(){
 }
 
 function CollegeDropDownMenu(){
+  let colleges = [
+    {label: "Agricultural Sciences", value: "ag-sci"},
+    {label: "Business", value: "business"},
+    {label: "Earth, Ocean, and Atmospheric Sciences", value: "earth-ocean-atmos-science"},
+    {label: "Education", value: "education"},
+    {label: "Engineering", value: "engineering"},
+    {label: "Forestry", value: "forestry"},
+    {label: "Liberal Arts", value: "liberal-arts"},
+    {label: "Pharmacy", value: "pharmacy"},
+    {label: "Public Health and Human Sciences", value: "public-health-human-science"},
+    {label: "Science", value: "science"},
+    {label: "Veterinary Medicine", value: "vet-med"},
+  ]
+
   const [college, setCollege] = React.useState('');
 
   const handleChange = (event) => {
     setCollege(event.target.value);
-
   };
   return (
-    <div>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-helper-label">College</InputLabel>
-        <Select
-          labelId="demo-simple-select-helper-label"
-          id="demo-simple-select-helper"
-          value={college}
-          label="College"
-          onChange={handleChange}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={"ag-sci"}>Agricultural Sciences</MenuItem>
-          <MenuItem value={"business"}>Business</MenuItem>
-          <MenuItem value={"eart-ocean-atmos-science"}>Earth, Ocean, and Atmospheric Sciences</MenuItem>
-          <MenuItem value={"education"}>Education</MenuItem>
-          <MenuItem value={"engineering"}>Engineering</MenuItem>
-          <MenuItem value={"forestry"}>Forestry</MenuItem>
-          <MenuItem value={"liberal-arts"}>Liberal Arts</MenuItem>
-          <MenuItem value={"pharmacy"}>Pharmacy</MenuItem>
-          <MenuItem value={"public-health-human-science"}>Public Health and Human Sciences</MenuItem>
-          <MenuItem value={"science"}>Science</MenuItem>
-          <MenuItem value={"vet-science"}>Veterinary Medicine</MenuItem>
-        </Select>
-      </FormControl>
-      {college === "engineering" ?
       <div>
-      {<JsonDataDisplay/>}
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-helper-label">College</InputLabel>
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={college}
+            label="College"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            {colleges.map((college) => <MenuItem value={college.value}>{college.label}</MenuItem>)}
+          </Select>
+        </FormControl>
+        {college === "engineering" ?
+        <div>
+        {<JsonDataDisplay/>}
+        </div>
+        :
+        <div></div>
+        }
       </div>
-      :
-      <div></div>
-      }
-    </div>
   );
 }
   
+
 function MajorDropDownMenu(){
+
+  let engineeringMajors = [
+    {label: "Architectual Engineering", value: "arch"},
+    {label: "Bioengineering", value: "bio"},
+    {label: "Chemical Engineering", value: "chem"},
+    {label: "Civil Engineering", value: "civil"},
+    {label: "Computer Science", value: "comp-sci"},
+    {label: "Construction Engineering Management", value: "construction-mgmt"},
+    {label: "Ecological Engineering", value: "eco"},
+    {label: "Electric and Computer Engineering", value: "electric"},
+    {label: "Energy Systems Engineering", value: "energy-sys"},
+    {label: "Environmental Engineering", value: "enviro"},
+    {label: "Industrial Engineering", value: "industrial"},
+    {label: "Manufacturing Engineering", value: "manufacturing"},
+    {label: "Mechanical Engineering", value: "mechanical"},
+    {label: "Nuclear Engineering", value: "nuclear"},
+    {label: "Radiation Health Physics", value: "radiation"},
+  ]
   const [major, setMajor] = React.useState('');
 
   const handleChange = (event) => {
@@ -93,22 +117,8 @@ function MajorDropDownMenu(){
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={"arch"}>Architectual Engineering</MenuItem>
-          <MenuItem value={"bio"}>Bioengineering</MenuItem>
-          <MenuItem value={"chem"}>Chemical Engineering</MenuItem>
-          <MenuItem value={"civil"}>Civil Engineering</MenuItem>
-          <MenuItem value={"compsci"}>Computer Science</MenuItem>
-          <MenuItem value={"construction-mgmt"}>Construction Engineering Management</MenuItem>
-          <MenuItem value={"eco"}>Ecological Engineering</MenuItem>
-          <MenuItem value={"electric"}>Electrical and Computer Engineering</MenuItem>
-          <MenuItem value={"energy-sys"}>Energy Systems Engineering</MenuItem>
-          <MenuItem value={"eng-sci"}>Engineering Science</MenuItem>
-          <MenuItem value={"enviro"}>Environmental Engineering</MenuItem>
-          <MenuItem value={"industrial"}>Industrial Engineering</MenuItem>
-          <MenuItem value={"manufacturing"}>Manufacturing Engineering</MenuItem>
-          <MenuItem value={"mechanical"}>Mechanical Engineering</MenuItem>
-          <MenuItem value={"nuclear"}>Nuclear Engineering</MenuItem>
-          <MenuItem value={"radiation"}>Radiation Health Physics</MenuItem>
+          {engineeringMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+          
         </Select>
       </FormControl>
     </div>
@@ -116,6 +126,18 @@ function MajorDropDownMenu(){
 }
   
 function OptionDropDownMenu(){
+  let csOptions =[
+    {label: "Cybersecurity", value:"cyber-sec"},
+    {label: "Data Science", value:"data"},
+    {label: "Artificial Intelligence", value:"ai"},
+    {label: "Robot Intelligence", value:"robot"},
+    {label: "Bioinformatics", value:"bio"},
+    {label: "Business and Entrepeneurship", value:"business"},
+    {label: "Human-Computer Interaction", value:"human-comp"},
+    {label: "Simulation and Game Programming", value:"game"},
+    {label: "Web and Mobile Application", value:"web"},
+    {label: "Systems", value:"sys"}
+  ]
   const [option, setOption] = React.useState('');
 
   const handleChange = (event) => {
@@ -135,16 +157,7 @@ function OptionDropDownMenu(){
           <MenuItem value="">
             <em>None</em>
           </MenuItem>
-          <MenuItem value={10}>Applied Option: Cybersecurity</MenuItem>
-          <MenuItem value={20}>Applied Option: Data Science</MenuItem>
-          <MenuItem value={30}>Applied Option: Artificial Intelligence</MenuItem>
-          <MenuItem value={40}>Applied Option: Robot Intelligence</MenuItem>
-          <MenuItem value={50}>Applied Option: Bioinformatics</MenuItem>
-          <MenuItem value={60}>Applied Option: Business and Entrepeneurship</MenuItem>
-          <MenuItem value={70}>Applied Option: Human-Computer Interaction</MenuItem>
-          <MenuItem value={80}>Applied Option: Simulation and Game Programming</MenuItem>
-          <MenuItem value={90}>Applied Option: Web and Mobile Application</MenuItem>
-          <MenuItem value={100}>Systems Option</MenuItem>
+          {csOptions.map((option) => <MenuItem value={option.value}>{option.label}</MenuItem>)}
         </Select>
       </FormControl>
     </div>
