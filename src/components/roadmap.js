8bd24c48ae27ebd26b6ce5111f1b95a9cc581c63
Isabelle Bretl <bretli@oregonstate.edu>
2022-02-20 15:02:51 -0800

    dropdown data put into json files and parsed accordingly

--- src/components/roadmap.js
+++ src/components/roadmap.js
@@ -27,70 +27,27 @@ function AppDisplay(){
 }
 
 function DropDowns(){
-  let colleges = [
-    {label: "Agricultural Sciences", value: "ag-sci"},
-    {label: "Business", value: "business"},
-    {label: "Earth, Ocean, and Atmospheric Sciences", value: "earth-ocean-atmos-science"},
-    {label: "Education", value: "education"},
-    {label: "Engineering", value: "engineering"},
-    {label: "Forestry", value: "forestry"},
-    {label: "Liberal Arts", value: "liberal-arts"},
-    {label: "Pharmacy", value: "pharmacy"},
-    {label: "Public Health and Human Sciences", value: "public-health-human-science"},
-    {label: "Science", value: "science"},
-    {label: "Veterinary Medicine", value: "vet-med"},
-  ]
-
+  //---------- variables for college selection -----------
+  const collegesJSON = require('./colleges.json');
+  var parsedColleges = collegesJSON.colleges;
   const [college, setCollege] = React.useState('');
-
-  const handleChange = (event) => {
+  const handleCollegeChange = (event) => {
     setCollege(event.target.value);
   };
-
-
-  let engineeringMajors = [
-    {label: "Architectual Engineering", value: "arch"},
-    {label: "Bioengineering", value: "bio"},
-    {label: "Chemical Engineering", value: "chem"},
-    {label: "Civil Engineering", value: "civil"},
-    {label: "Computer Science", value: "comp-sci"},
-    {label: "Construction Engineering Management", value: "construction-mgmt"},
-    {label: "Ecological Engineering", value: "eco"},
-    {label: "Electric and Computer Engineering", value: "electric"},
-    {label: "Energy Systems Engineering", value: "energy-sys"},
-    {label: "Environmental Engineering", value: "enviro"},
-    {label: "Industrial Engineering", value: "industrial"},
-    {label: "Manufacturing Engineering", value: "manufacturing"},
-    {label: "Mechanical Engineering", value: "mechanical"},
-    {label: "Nuclear Engineering", value: "nuclear"},
-    {label: "Radiation Health Physics", value: "radiation"},
-  ]
+  //---------- variables for major selection -----------
+  const majorsJSON = require('./majors.json');
+  var parsedEngineeringMajor = majorsJSON.engineeringMajors;
   const [major, setMajor] = React.useState('');
-
   const handleMajorChange = (event) => {
     setMajor(event.target.value);
   };
-
-
-  let csOptions =[
-    {label: "Cybersecurity", value:"cyber-sec"},
-    {label: "Data Science", value:"data"},
-    {label: "Artificial Intelligence", value:"ai"},
-    {label: "Robot Intelligence", value:"robot"},
-    {label: "Bioinformatics", value:"bio"},
-    {label: "Business and Entrepeneurship", value:"business"},
-    {label: "Human-Computer Interaction", value:"human-comp"},
-    {label: "Simulation and Game Programming", value:"game"},
-    {label: "Web and Mobile Application", value:"web"},
-    {label: "Systems", value:"sys"}
-  ]
+  //---------- variables for option selection -----------
+  const optionsJSON = require('./options.json');
+  var parsedCSOption = optionsJSON.compScienceOptions;
   const [option, setOption] = React.useState('');
-
   const handleOptionChange = (event) => {
     setOption(event.target.value);
   };
-
-
   return (
       <div>
         <FormControl sx={{ m: 1, minWidth: 120 }}>
@@ -100,12 +57,12 @@ function DropDowns(){
             id="demo-simple-select-helper"
             value={college}
             label="College"
-            onChange={handleChange}
+            onChange={handleCollegeChange}
           >
             <MenuItem value="">
               <em>None</em>
             </MenuItem>
-            {colleges.map((college) => <MenuItem value={college.value}>{college.label}</MenuItem>)}
+            {parsedColleges.map((college) => <MenuItem value={college.value}>{college.label}</MenuItem>)}
           </Select>
         </FormControl>
         {college !== '' ?
@@ -123,8 +80,7 @@ function DropDowns(){
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
-                {engineeringMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
-                
+                {parsedEngineeringMajor.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
               </Select>
             </FormControl>
             {major === 'comp-sci' ?
@@ -142,7 +98,7 @@ function DropDowns(){
                       <MenuItem value="">
                         <em>None</em>
                       </MenuItem>
-                      {csOptions.map((option) => <MenuItem value={option.value}>{option.label}</MenuItem>)}
+                      {parsedCSOption.map((option) => <MenuItem value={option.value}>{option.label}</MenuItem>)}
                     </Select>
                   </FormControl>
                 </div>
