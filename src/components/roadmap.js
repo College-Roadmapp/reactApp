0df96629955f1af153092c42815f3770be8ef2da
Isabelle Bretl <bretli@oregonstate.edu>
2022-02-20 16:45:46 -0800

    filled in all college's majors in majors.json and added functionality for major dropdown to show all majors given the selected college correctly

--- src/components/roadmap.js
+++ src/components/roadmap.js
@@ -26,6 +26,11 @@ function AppDisplay(){
   )
 }
 
+
+
+//notes:
+//couldnt render only the map MenuItem components because the conditional
+//statements need a parent component
 function DropDowns(){
   //---------- variables for college selection -----------
   const collegesJSON = require('./colleges.json');
@@ -36,7 +41,17 @@ function DropDowns(){
   };
   //---------- variables for major selection -----------
   const majorsJSON = require('./majors.json');
-  var parsedEngineeringMajor = majorsJSON.engineeringMajors;
+  var parsedMajors = majorsJSON.majors;
+  var parsedAgSciMajors = parsedMajors.agSciMajors;
+  var parsedBusMajors = parsedMajors.businessMajors;
+  var parsedEOASMajors = parsedMajors.EOASMajors;
+  var parsedEdMajors = parsedMajors.educationMajors;
+  var parsedEngMajors = parsedMajors.engineeringMajors;
+  var parsedForestryMajors = parsedMajors.forestryMajors;
+  var parsedLibArtsMajors = parsedMajors.libArtsMajors;
+  var parsedPharmMajors = parsedMajors.pharmacyMajors;
+  var parsedPubHealthMajors = parsedMajors.pubHealthMajors;
+  var parsedSciMajors = parsedMajors.scienceMajors;
   const [major, setMajor] = React.useState('');
   const handleMajorChange = (event) => {
     setMajor(event.target.value);
@@ -68,21 +83,170 @@ function DropDowns(){
         {college !== '' ?
         <div>
           <div>
-            <FormControl sx={{ m: 1, minWidth: 120 }}>
-              <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={major}
-                label="Major"
-                onChange={handleMajorChange}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                {parsedEngineeringMajor.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
-              </Select>
-            </FormControl>
+            {college === "ag-sci" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedAgSciMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "business" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedBusMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "earth-ocean-atmos-science" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedEOASMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "education" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedEdMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "engineering" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedEngMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "forestry" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedForestryMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "liberal-arts" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedLibArtsMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "pharmacy" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedPharmMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "public-health-human-science" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedPubHealthMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            }
+            {college === "science" &&
+              <FormControl sx={{ m: 1, minWidth: 120 }}>
+                <InputLabel id="demo-simple-select-helper-label">Major</InputLabel>
+                  <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="demo-simple-select-helper"
+                  value={major}
+                  label="Major"
+                  onChange={handleMajorChange}>
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                      {parsedSciMajors.map((major) => <MenuItem value={major.value}>{major.label}</MenuItem>)}
+                    </Select> 
+              </FormControl>
+            } 
+
+
+
+
             {major === 'comp-sci' ?
               <div>
                 <div>
