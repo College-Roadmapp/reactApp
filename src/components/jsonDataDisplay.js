05848fa349257a99c40b596311d29e2e7980782a
Isabelle Bretl <bretli@oregonstate.edu>
2022-02-23 21:11:18 -0800

    added 'change term' button to class component with modal popup + dropdown menu, and a checkbox to indicate if that class has been taken

--- src/components/jsonDataDisplay.js
+++ src/components/jsonDataDisplay.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import { FormControl } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import React, { Component, useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import InputLabel from '@mui/material/InputLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 
 class Course {
@@ -54,6 +63,59 @@ class Degree {
 }
 
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+function BasicModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const allTerms = [1,2,3,4,5,6,7,8,9,10,11,12]
+  const [term, setTerm] = React.useState(0)
+  const handleTermChange = event => {
+    setTerm(event.target.value);
+  }
+  return (
+    <div>
+      <Button onClick={handleOpen}>Change Term</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Which term would you like to move this class to?
+          </Typography>
+          <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-helper-label">Term</InputLabel>
+            <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={term}
+            label="Term"
+            onChange={handleTermChange}
+            >
+              {allTerms.map((term) => <MenuItem value={term}>{term}</MenuItem>)}
+            </Select>
+          </FormControl>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
 
 class Table {
   constructor(objectArray){
@@ -72,15 +134,20 @@ class Table {
         temp.push(this.array.getCourse(i));
       }
     }
-
     temp=temp.map(
         (info, i)=>{
             return(
                 <tr>
+                    <td>
+                      <Checkbox/>
+                    </td>
                     <td>{info.id}</td>
                     <td>{info.name}</td>
                     <td>{info.credits}</td>
                     <td>{info.term}</td>
+                    <td>
+                      <BasicModal/>
+                    </td>
                 </tr>
             )
         }
@@ -133,11 +200,11 @@ function JsonDataDisplay(){
 
   let newDegree = IntoClassObjects();
   let newTable1 = new Table(newDegree);
-  let newTable2 = new Table(newDegree);
-  let newTable3 = new Table(newDegree);
-  let newTable4 = new Table(newDegree);
-  let newTable5 = new Table(newDegree);
-  let newTable6 = new Table(newDegree);
+  // let newTable2 = new Table(newDegree);
+  // let newTable3 = new Table(newDegree);
+  // let newTable4 = new Table(newDegree);
+  // let newTable5 = new Table(newDegree);
+  // let newTable6 = new Table(newDegree);
 
 
   let var1 = (
